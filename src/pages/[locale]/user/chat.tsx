97b296d6efc65955cbc815a7decbbb0e6de7aa73
KishiTheMechanic <s.kishi@elsoul.nl>
2023-07-28 19:20:11 +0200

    refetch screen

--- src/pages/[locale]/user/chat.tsx
+++ src/pages/[locale]/user/chat.tsx
@@ -13,6 +13,7 @@ import {
 } from '@/__generated__/ChatScreenQuery.graphql'
 import { useQueryLoader } from 'react-relay'
 import { sleep } from '@/utils/time'
+import RefetchChat from '@/components/pages/user/chat/RefetchChat'
 
 const seo = {
   pathname: '/user/chat',
@@ -63,11 +64,15 @@ export default function Chat() {
   }
   return (
     <>
-      <Suspense fallback={<UserScreenLoading />}>
-        <UserScreenErrorBoundary showRetry={<p>error</p>}>
-          <ChatScreen queryReference={queryReference} refetch={refetch} />
-        </UserScreenErrorBoundary>
-      </Suspense>
+      <div className="h-screen">
+        <Suspense fallback={<UserScreenLoading />}>
+          <UserScreenErrorBoundary
+            showRetry={<RefetchChat refetch={refetch} />}
+          >
+            <ChatScreen queryReference={queryReference} refetch={refetch} />
+          </UserScreenErrorBoundary>
+        </Suspense>
+      </div>
     </>
   )
 }
