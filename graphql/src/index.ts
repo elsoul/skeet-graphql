cce17cbdb1d8ae37f24d4979b417b61baa093c25
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2023-07-29 12:45:39 +0200

    fix skeetGraphql

--- graphql/src/index.ts
+++ graphql/src/index.ts
@@ -67,7 +67,11 @@ export const server = new ApolloServer<Context>({
   introspection: true,
 })
 
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:4200']
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:4200',
+  'https://skeet-graphql.web.app/',
+]
 new Array(10).fill(0).forEach((_, i) => {
   allowedOrigins.push(`http://localhost:1900${i}`)
 })
